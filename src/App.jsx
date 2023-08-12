796b9e241a0135acd11330e322d8876480775489
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-08-12 20:23:53 +0630

    create form and listing components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from './components/form';
+import Listing from './components/listing';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <Form />
+    <Listing />
+  </div>
+);
+
+export default App;
