c7689c398a87e7349850cb9a857dfffb75ba12d8
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-08-13 23:33:32 +0630

    create index.js to store urlsByCategory

---
+++ src/index.js
@@ -0,0 +1,23 @@
+export const urlsByCategory = [
+    {
+        "name": "Lenovo",
+        "urls": [
+            "https://www.flipkart.com/lenovo-23-8-inch-full-hd-va-panel-3-side-near-edgeless-tuv-eye-care-monitor-d24-20/p/itm8f8c6bfc86fb5?pid=MONFV5HRNF4QFVG4&lid=LSTMONFV5HRNF4QFVG4ATFEMN&marketplace=FLIPKART&store=6bo/g0i/9no&srno=b_1_1&otracker=clp_metro_expandable_2_3.metroExpandable.METRO_EXPANDABLE_LG_monitors-store_04FG6HZUUFKV_wp3&fm=neo/merchandising&iid=en_cqpDeGPALSku136Z1OmI0zH1wAQfWAajyx0QrRsORSsfhHAsfBHd3NjxwLzC9qP3wgT8V7eVxEeZUI7AVs5yGg==&ppt=clp&ppn=monitors-store&ssid=lx688hi15c0000001691764371155",
+            "https://www.flipkart.com/lenovo-27-inch-full-hd-va-panel-tuv-eye-care-monitor-d27-40-d27-30/p/itm20eeaca295f0b?pid=MONFXZREMZYTJGNG&lid=LSTMONFXZREMZYTJGNGYRB0VL&marketplace=FLIPKART&fm=productRecommendation%2Fsimilar&iid=en_D5GcgiCSp73dmEVEczOqoqUT4bMIg3OlUhLCOif7UROh64n7yM1cZzjV_qa6QR7TwK19SEyMTP5sqzWG_Ni7lA%3D%3D&ppt=pp&ppn=pp&ssid=lx688hi15c0000001691764371155&otracker=pp_reco_Similar%2BProducts_2_37.productCard.PMU_HORIZONTAL_Lenovo%2B27%2Binch%2BFull%2BHD%2BVA%2BPanel%2Bwith%2BTUV%2BEye%2BCare%2BMonitor%2B%2528D27-40%252FD27-30%2529_-1_productRecommendation%2Fsimilar_1&otracker1=pp_reco_PINNED_productRecommendation%2Fsimilar_Similar%2BProducts_GRID_productCard_cc_2_NA_view-all&cid=-1",
+        ]
+    },
+    {
+        "name": "LG",
+        "urls": [
+            "https://www.flipkart.com/lg-24-inch-full-hd-led-backlit-ips-panel-white-colour-monitor-24mk600m/p/itmfb6jq2jgffrtj?pid=MONFB6JQFJEP7BZX&lid=LSTMONFB6JQFJEP7BZXRIOGNF&marketplace=FLIPKART&fm=productRecommendation%2Fsimilar&iid=R%3As%3Bp%3AMONFV5HRNF4QFVG4%3Bl%3ALSTMONFV5HRNF4QFVG4ATFEMN%3Bpt%3App%3Buid%3A0bc63ea9-39d6-11ee-ab61-1f738cc21b53%3B.MONFB6JQFJEP7BZX&ppt=pp&ppn=pp&ssid=lx688hi15c0000001691764371155&otracker=pp_reco_Similar%2BProducts_25_LG%2B24%2Binch%2BFull%2BHD%2BLED%2BBacklit%2BIPS%2BPanel%2BWhite%2BColour%2BMonitor%2B%252824MK600M%2529_MONFB6JQFJEP7BZX_productRecommendation%2Fsimilar_24&otracker1=pp_reco_NA_productRecommendation%2Fsimilar_Similar%2BProducts_DESKTOP_HORIZONTAL_productCard_cc_25_NA_view-all&cid=MONFB6JQFJEP7BZX",
+            "https://www.flipkart.com/lg-21-5-inch-full-hd-ips-panel-ultra-thin-monitor-22mk600m/p/itm297ef0f41b753?pid=MONFK842GG8RUHQQ&lid=LSTMONFK842GG8RUHQQXTTDOW&marketplace=FLIPKART&fm=productRecommendation%2Fsimilar&iid=R%3As%3Bp%3AMONFB6JQFJEP7BZX%3Bl%3ALSTMONFB6JQFJEP7BZXRIOGNF%3Bpt%3App%3Buid%3A27ab54d3-39f3-11ee-9ad7-ade76d6e751d%3B.MONFK842GG8RUHQQ&ppt=pp&ppn=pp&ssid=lx688hi15c0000001691764371155&otracker=pp_reco_Similar%2BProducts_6_37.productCard.PMU_HORIZONTAL_LG%2B21.5%2Binch%2BFull%2BHD%2BIPS%2BPanel%2BUltra%2BThin%2BMonitor%2B%252822MK600M%2529_MONFK842GG8RUHQQ_productRecommendation%2Fsimilar_5&otracker1=pp_reco_PINNED_productRecommendation%2Fsimilar_Similar%2BProducts_GRID_productCard_cc_6_NA_view-all&cid=MONFK842GG8RUHQQ",
+        ]
+    },
+    {
+        "name": "SAMSUNG",
+        "urls": [
+            "https://www.flipkart.com/samsung-22-inch-full-hd-ips-panel-monitor-ls22c310eawxxl/p/itmac31067559ef1?pid=MONGAQTQKCEZGVSC&lid=LSTMONGAQTQKCEZGVSCG4LB7V&marketplace=FLIPKART&fm=productRecommendation%2Fsimilar&iid=R%3As%3Bp%3AMONFV5HRNF4QFVG4%3Bl%3ALSTMONFV5HRNF4QFVG4ATFEMN%3Bpt%3App%3Buid%3A0bc63ea9-39d6-11ee-ab61-1f738cc21b53%3B.MONGAQTQKCEZGVSC&ppt=pp&ppn=pp&ssid=lx688hi15c0000001691764371155&otracker=pp_reco_Similar%2BProducts_12_SAMSUNG%2B22%2Binch%2BFull%2BHD%2BIPS%2BPanel%2BMonitor%2B%2528LS22C310EAWXXL%2529_MONGAQTQKCEZGVSC_productRecommendation%2Fsimilar_11&otracker1=pp_reco_NA_productRecommendation%2Fsimilar_Similar%2BProducts_DESKTOP_HORIZONTAL_productCard_cc_12_NA_view-all&cid=MONGAQTQKCEZGVSC",
+            "https://www.flipkart.com/samsung-m5-32-inch-full-hd-led-backlit-va-panel-embedded-tv-apps-pc-less-productivity-dex-office-365-google-duo-app-iot-hub-built-in-speakers-ultrawide-game-view-smart-monitor-ls32cm500ewxxl/p/itmf37a817093719?pid=MONGEWMGWFVXZTDX&lid=LSTMONGEWMGWFVXZTDXUOWFWF&marketplace=FLIPKART&q=samsung+monitor&store=6bo%2Fg0i%2F9no&srno=s_1_10&otracker=search&otracker1=search&fm=productRecommendation%2Fsimilar&iid=f7ef495f-031d-409a-91cf-e4cb3e0ed9c4.MONGEWMGWFVXZTDX.SEARCH&ppt=pp&ppn=pp&ssid=1c96cgfg9s0000001691946113845&qH=0f67401d930ec292",
+        ]
+    }, 
+]
