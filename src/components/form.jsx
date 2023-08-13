5194969bdd25b81d11611d7d0a8fffd72e02a34d
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-08-13 23:22:43 +0630

    display the scraped data in the form component

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -5,6 +5,12 @@ import { fetchProducts } from '../redux/productsSlice';
 const Form = () => {
   const dispatch = useDispatch();
   const [url, setUrl] = useState('');
+  const category = useSelector(state => state.product.categories);
+  const product = useSelector(state => state.product.products);
+  const img = useSelector(state => state.product.images);
+  console.log('category', category);
+  console.log('product', product);
+  console.log('img', img);
   const isLoading = useSelector(state => state.product.isLoading);
 
   const handleUrlChange = (event) => {
@@ -15,7 +21,7 @@ const Form = () => {
     if (url.trim() !== '') {
       console.log('Sending request...');
       setTimeout(() => {
-        dispatch(fetchProducts({ url: url, categoryName: 'Lenovo' }));
+        dispatch(fetchProducts({ url: url, categoryName: 'LG' }));
         console.log('Request sent!'); 
       }, 3000);
     }
@@ -23,19 +29,34 @@ const Form = () => {
 
   return (
     <div>
-      <input
-        type="text"
-        value={url}
-        onChange={handleUrlChange}
-        placeholder="Enter URL to scrape the data"
-      />
-      <button
-        type="button"
-        onClick={handleFetchClick}
-        disabled={false}
-      >
-        {isLoading ? 'Fetching...' : 'Fetch'}
-      </button>
+      <div>
+        <input
+          type="text"
+          value={url}
+          onChange={handleUrlChange}
+          placeholder="Enter URL to scrape the data"
+        />
+        <button
+          type="button"
+          onClick={handleFetchClick}
+          disabled={false}
+        >
+          {isLoading ? 'Fetching...' : 'Fetch'}
+        </button>
+      </div>
+      <div>
+        <div>
+          <img src={img} alt={category.name} />
+        </div>
+        <div>
+          <h2>Category: {category.name}</h2>
+          <h3>Title: {product.title}</h3>
+          <p>Description: {product.description}</p>
+          <p>Price: {product.price}</p>
+          <p>Model_year: {product.model_year}</p>
+          <p>Size: {product.size}</p>
+        </div>
+      </div>
     </div>
   );
 };
